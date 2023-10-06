008102a4b013ef010bb313fb258f2be5f4daf8c3
jvsobraz <jvsobraz@hotmail.com>
2023-10-05 21:00:42 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# API - É um lugar para disponibilizar recursos e/ou funcionalidades.
+
+# 1. Objetivo - Criar uma API que disponibiliza a consulta, criação, edição e exclusão de livros.
+# 2. URL base - localhost
+# 3. Endpoints - 
+    # - localhost/livros (GET)
+    # - localhost/livros (POST)
+    # - localhost/livros/id (GET)
+    # - localhost/livros/id (PUT)
+    # - localhost/livros/ (DELETE)
+# 4. Quais recursos? - Livros
+
